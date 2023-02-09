b3f81a9e724e882e3d23e7ea247a19504c3d59d9
whyisy <cjg0504@pusan.ac.kr>
2023-02-09 12:18:13 +0900

    1.할일목록리스팅

---
+++ src/components/TodoMain.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="page">
+    <header><h1>Vue Fire todo1</h1></header>
+    <main>
+      <div class="todos">
+        <div class="write">
+          <input type="text"/>
+          <button class="btn add">Add</button>
+        </div>
+        <ul class="list">
+          <li v-for="(todo,i) in todos" :key="i">
+            <i class="far fa-check-square"></i>
+            <span>
+                <li>
+                    <span>{{ todo.text }}</span>
+                </li>
+              <b>
+                <a href="">Edit</a>
+                <a href="">Del</a>
+              </b>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </main>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        todos:[
+        {text: '공부하기', state: 'yet'},
+        {text: '운동하기', state: 'done'},
+        {text: '글쓰기', state: 'done'},
+      ]
+        }
+    },
+    props: ['todoList'],
+}
+</script>
+
+<style>
