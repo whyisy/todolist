c9a0f6a68f9f80a2e7c608132c5822e62e1e612c
whyisy <cjg0504@pusan.ac.kr>
2023-02-09 14:14:16 +0900

    2. 할일 등록 기능

--- src/components/TodoMain.vue
+++ src/components/TodoMain.vue
@@ -1,19 +1,17 @@
 <template>
-    <div class="page">
+        <div class="page">
     <header><h1>Vue Fire todo1</h1></header>
     <main>
       <div class="todos">
         <div class="write">
-          <input type="text"/>
-          <button class="btn add">Add</button>
+          <input type="text" v-model="addItemText" @keyup.enter="addItem"/>
+          <button class="btn add" @click="addItem">Add</button>
         </div>
         <ul class="list">
-          <li v-for="(todo,i) in todos" :key="i">
-            <i class="far fa-check-square"></i>
+          <li v-for="(item, i) in todos" :key="i" >
+            <i :class="[item.state === 'yet' ? 'far' : 'fas', 'fa-check-square']"></i>
             <span>
-                <li>
-                    <span>{{ todo.text }}</span>
-                </li>
+                {{ item.text }}
               <b>
                 <a href="">Edit</a>
                 <a href="">Del</a>
@@ -30,15 +28,24 @@
 export default {
     data() {
         return {
-        todos:[
-        {text: '공부하기', state: 'yet'},
-        {text: '운동하기', state: 'done'},
-        {text: '글쓰기', state: 'done'},
-      ]
+            addItemText:'',
+            todos:[
+                {text: '공부하기', state: 'yet'},
+                {text: '운동하기', state: 'done'},
+                {text: '글쓰기', state: 'done'},
+            ]
         }
     },
-    props: ['todoList'],
+    props: ['todoData'],
+    methods: {
+        addItem() {
+            if (this.addItemText === '') return;
+            this.todo.push({text: this.addItemText, state: 'yet'}),
+            this.addItemText='';
+        }
+    }
 }
 </script>
 
 <style>
+</style>
